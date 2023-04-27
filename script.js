be57543b9bb184bfe3a2dc9de50c7d15b8c26308
Anthony Santos <anthony.santos1824@gmail.com>
2023-04-26 22:14:06 -0400

    update
    update

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var object;
 
+
+var turns= 0 ;
+var count= 0;
 function createSquare() {
   var square = document.getElementById("square");
   for (var i = 0; i < 100; i++) {
@@ -17,47 +20,98 @@ function createObject() {
 }
 
 function moveObject() {
+
+  
   var currentPositionX = parseInt(object.style.left) || 0;
   var currentPositionY = parseInt(object.style.top) || 0;
   var nextPositionX = currentPositionX;
   var nextPositionY = currentPositionY;
   
  
-  if (currentPositionY == 0 && currentPositionX < 180) {
+  
+  
+  if (currentPositionY == 0 && currentPositionX < 180) {    //right
     nextPositionX += 20;
-  }
-  if (currentPositionX == 180 && currentPositionY == 0) {
+  }if (currentPositionX == 180 && currentPositionY == 0) {  //down
     nextPositionY = 20;
-  } 
-  if (currentPositionY == 20 && currentPositionX > 0) {
+    turns +=1;
+  }if (currentPositionY == 20 && currentPositionX > 0) {    //left
     nextPositionX -= 20;
-  } 
-  if ( currentPositionX == 0 && currentPositionY == 20) {
-    nextPositionY = 40;
-    
-  } 
-  
-  if (currentPositionY == 40 && currentPositionX < 180){
-    nextPositionX +=20;
-    
+  }if ( currentPositionX == 0 && currentPositionY == 20) {  //down 
+    nextPositionY = 40;  
+    turns++;
+  }if (currentPositionY == 40 && currentPositionX < 180){   //right
+    nextPositionX +=20; 
   }
-  if(currentPositionX == 180 && currentPositionY == 40 ){
+
+
+  if(currentPositionX == 180 && currentPositionY == 40 ){   //down
     nextPositionY = 60;
+    turns++;
   }
-  if (currentPositionY == 60 && currentPositionX > 0) {
+  if (currentPositionY == 60 && currentPositionX > 0) {   //left
     nextPositionX -= 20;
   }
-  if ( currentPositionX == 0 && currentPositionY == 60) {
+  if ( currentPositionX == 0 && currentPositionY == 60) {   //down
     nextPositionY = 80;
+    turns++;
   }
-  if (currentPositionY == 80 && currentPositionX < 180){
+
+
+  if (currentPositionY == 80 && currentPositionX < 180){        //right
+    nextPositionX +=20;
+  } if (currentPositionX == 180 && currentPositionY == 80) {    //down 
+    nextPositionY = 100;
+    turns++;
+  } if (currentPositionY == 100 && currentPositionX > 0) {       //left
+    nextPositionX -= 20;
+  } if ( currentPositionX == 0 && currentPositionY == 100) {      //down
+    nextPositionY = 120;  
+    turns++;
+  } if (currentPositionY == 120 && currentPositionX < 180){       //right
     nextPositionX +=20;
   }
-  
-  
+    
 
 
+  if (currentPositionX == 180 && currentPositionY == 120) {    //down 
+    nextPositionY = 140;
+    turns++;
+  } 
+
+
+  if (currentPositionY == 140 && currentPositionX > 0) {       //left
+    nextPositionX -= 20;
+  }
+
+
+  if ( currentPositionX == 0 && currentPositionY == 140) {      //down
+    nextPositionY = 160;  
+    turns++;
+  } 
+  if (currentPositionY == 160 && currentPositionX < 180){       //right
+    nextPositionX +=20;
+  }
+  if (currentPositionY == 160 && currentPositionX == 180){
+    nextPositionY = 180;
+    turns++;
+  }
+  if(currentPositionY == 180 && currentPositionX > 0){
+    nextPositionX-= 20;
+  }
+
+  
   object.style.left = nextPositionX + "px";
   object.style.top = nextPositionY + "px";
-  setTimeout(moveObject, 500);
+  setTimeout(moveObject, 100);
+
+ 
+  
+  var outputBox = document.getElementById("output");
+  outputBox.innerHTML = "Number of turns: " + turns; 
+
+  
+  
 }
+
+
